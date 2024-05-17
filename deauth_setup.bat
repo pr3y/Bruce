4ba9bb9e98ae76187f704c1d64c846b120ca327a
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-05-17 16:07:41 -0300

    Prepare to Deauth

--- deauth_setup.bat
+++ deauth_setup.bat
@@ -4,9 +4,11 @@ setlocal enabledelayedexpansion
 rem Define file paths
 set "esp32_file=%userprofile%\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32\lib\libnet80211.a"
 set "esp32s3_file=%userprofile%\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32s3\lib\libnet80211.a"
+set "esp32s3_file3=%userprofile%\.platformio\packages\framework-arduinoespressif32@3.20014.231204\tools\sdk\esp32s3\lib\libnet80211.a"
 
 set "esp32_file2=%userprofile%\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32\lib\libnet80211_temp.a"
 set "esp32s3_file2=%userprofile%\.platformio\packages\framework-arduinoespressif32\tools\sdk\esp32s3\lib\libnet80211_temp.a"
+set "esp32s3_file4=%userprofile%\.platformio\packages\framework-arduinoespressif32@3.20014.231204\tools\sdk\esp32s3\lib\libnet80211_temp.a"
 
 rem Now execute objcopy commands (only if backups were created)
 "%userprofile%"\.platformio\packages\toolchain-xtensa-esp32\xtensa-esp32-elf\bin\objcopy --weaken-symbol=ieee80211_raw_frame_sanity_check "%esp32_file%" "%esp32_file2%"
@@ -25,6 +27,15 @@ ren "%esp32s3_file%" "libnet80211.a.old"
 
 rem Rename the _temp to original
 ren "%esp32s3_file2%" "libnet80211.a"
+
+rem Now execute objcopy commands (only if backups were created)
+"%userprofile%"\.platformio\packages\toolchain-xtensa-esp32s3\xtensa-esp32s3-elf\bin\objcopy --weaken-symbol=ieee80211_raw_frame_sanity_check "%esp32s3_file3%" "%esp32s3_file4%"
+
+rem Rename the original file to .old
+ren "%esp32s3_file3%" "libnet80211.a.old"
+
+rem Rename the _temp to original
+ren "%esp32s3_file4%" "libnet80211.a"
 
 PAUSE
 
--- deauth_setup.sh
+++ deauth_setup.sh
@@ -6,9 +6,11 @@ pio pkg install -p espressif32 -t toolchain-xtensa32
 
 esp32_file="$HOME/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/lib/libnet80211.a"
 esp32s3_file="$HOME/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/lib/libnet80211.a"
+esp32s3_file2="$HOME/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/lib/libnet80211.a"
 
 esp32_file_temp="$HOME/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/lib/libnet80211_temp.a"
 esp32s3_file_temp="$HOME/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/lib/libnet80211_temp.a"
+esp32s3_file_temp2="$HOME/.platformio/packages/framework-arduinoespressif32@3.20014.231204/tools/sdk/esp32s3/lib/libnet80211_temp.a"
 
 # Definir caminhos para as ferramentas objcopy
 toolchain_esp32="$HOME/.platformio/packages/toolchain-xtensa-esp32/xtensa-esp32-elf/bin/objcopy"
@@ -33,6 +35,15 @@ if [[ -f "$esp32_file" && -f "$esp32s3_file" ]]; then
 
     # Rename the _temp to original
     mv "$esp32s3_file_temp" "$esp32s3_file"
+
+    # Execute objcopy commands for ESP32-S3
+    $toolchain_esp32s3 --weaken-symbol=ieee80211_raw_frame_sanity_check "$esp32s3_file2" "$esp32s3_file_temp2"
+
+    # Rename the original file to .old
+    mv "$esp32s3_file2" "${esp32s3_file2}.old"
+
+    # Rename the _temp to original
+    mv "$esp32s3_file_temp2" "$esp32s3_file2"
 
     echo "Done."
 else
