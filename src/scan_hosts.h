e7ebecb7a7f99d148eb71365926e2b9a69d1b3ad
pr3y <github@prey.rocks>
2024-05-19 04:11:35 -0300

    improved host scan

--- src/scan_hosts.h
+++ src/scan_hosts.h
@@ -1,5 +1,10 @@
 
 #include <WiFi.h>
+#include <ESP32Ping.h>
+
+void ping_sweep();
+
+bool ping(IPAddress target);
 
 int etharp_get_entry(size_t i, ip4_addr_t **ipaddr, struct netif **netif, struct eth_addr **eth_ret);
 
