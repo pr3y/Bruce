b988b6dbbd1cbae8cb02ecf431b0d2e4b5d439d4
Valentin <valentin.dole@protonmail.com>
2024-08-01 18:41:46 +0200

    BLE Beacon / Scan and Mic spectrum only for M5StickC Plus 2

--- src/core/main_menu.cpp
+++ src/core/main_menu.cpp
@@ -70,7 +70,7 @@ void wifiOptions() {
 **********************************************************************/
 void bleOptions() {
   options = {
-    #if defined(STICK_C_PLUS) || defined(STICK_C_PLUS2)
+    #if defined(STICK_C_PLUS2)
     {"BLE Beacon",  [=]() { ble_test(); }},
     {"BLE Scan",     [=]() { ble_scan(); }},
     #endif
@@ -143,7 +143,7 @@ void irOptions(){
 **********************************************************************/
 void otherOptions(){
   options = {
-    #if defined(STICK_C_PLUS) || defined(STICK_C_PLUS2)
+    #if defined(STICK_C_PLUS2)
     {"Mic Spectrum", [=]() { mic_test(); }},
     #endif
     {"QRCodes",      [=]() { qrcode_menu(); }},
