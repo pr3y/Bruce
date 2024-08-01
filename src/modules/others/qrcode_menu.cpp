11f6c43306e3de5abd1135789422e77d5f38c9cb
Rennan Cockles <r3ck.dev@gmail.com>
2024-08-01 19:26:44 -0300

    accept float amount for pix qrcode

--- src/modules/others/qrcode_menu.cpp
+++ src/modules/others/qrcode_menu.cpp
@@ -48,8 +48,8 @@ void custom_qrcode() {
 void pix_qrcode() {
     String key = keyboard("", 25, "PIX Key:");
     String key_length = key.length() >= 10 ? String(key.length()) : "0" + String(key.length());
-    String amount = keyboard("1000", 10, "Int amount:");
-    amount = String(amount.toInt()) + ".00";
+    String amount = keyboard("1000.00", 10, "Int amount:");
+    amount = String(amount.toFloat());
     String amount_length = amount.length() >= 10 ? String(amount.length()) : "0" + String(amount.length());
 
     String data0 = "0014BR.GOV.BCB.PIX01" + key_length + key;
