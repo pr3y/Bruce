8e308a505b6236bf873cd35aa386d8806c3e93e1
Valentin <valentin.dole@protonmail.com>
2024-08-02 15:50:08 +0200

    Add Palnagotchi as a library

---
+++ lib/M5_Palnagotchi/palnagotchi/ui.h
@@ -0,0 +1,14 @@
+#include <Keyboard.h>
+#include "M5Cardputer.h"
+extern Keyboard_Class Keyboard;
+#include "mood.h"
+#include "pwngrid.h"
+
+void initUi();
+void wakeUp();
+void drawMood(String face, String phrase, bool broken = false);
+void drawTopCanvas();
+void drawBottomCanvas(uint8_t friends_run = 0, uint8_t friends_tot = 0,
+                      String last_friend_name = "", signed int rssi = -1000);
+void drawMenu();
+void updateUi(bool show_toolbars = false);
