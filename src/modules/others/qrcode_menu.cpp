6461846270e931fc31a7eff000978489a552eb51
Valentin <valentin.dole@protonmail.com>
2024-08-07 15:16:03 +0200

    Fix minor ambiguous press button while printing QRCodes

--- src/modules/others/qrcode_menu.cpp
+++ src/modules/others/qrcode_menu.cpp
@@ -37,6 +37,7 @@ void qrcode_display(String qrcodeUrl) {
     qrcode.init();
 
     qrcode.create(qrcodeUrl);
+    delay(300); //Due to M5 sel press, it could be confusing with next line
     while(!checkEscPress() && !checkSelPress()) delay(100);
     tft.fillScreen(BGCOLOR);
 #endif
