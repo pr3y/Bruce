a7a47a340fd314872088028313fbfcad0e2dd991
eadmaster <925171+eadmaster@users.noreply.github.com>
2024-08-10 17:20:53 +0200

    added gpio serial cmds

--- src/core/serialcmds.cpp
+++ src/core/serialcmds.cpp
@@ -47,6 +47,29 @@ void startSerialCommandsHandlerTask() {
 }
 
 
+bool is_free_gpio_pin(int pin_no ){
+  // check if pin_no is usable for general GPIO
+  std::vector<int> usable_pins = {GROVE_SDA, GROVE_SCL};
+
+  #if defined(STICK_C_PLUS2) || defined(STICK_C_PLUS)
+    usable_pins.insert(usable_pins.end(), { 25, 26, 32, 33, 0 });
+  #elif defined(ESP32S3DEVKITC1)
+    usable_pins.insert(usable_pins.end(), {
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  // GPIO1 to GPIO25
+      33,                                                                                        // GPIO33
+      38, 39, 40, 41, 42, 43, 44,                                                                // GPIO38 to GPIO44
+      47, 48                                                                                     // GPIO47 to GPIO48
+    });
+  #endif
+
+  for (int usable_pin : usable_pins)
+    if (pin_no == usable_pin )
+      return true;
+  // else
+  return false;
+}
+
+
 void SerialPrintHexString(uint64_t val) {
   char s[18] = {0};
   //snprintf(s, 10, "%x", val);
@@ -229,7 +252,7 @@ bool processSerialCommand(String cmd_str) {
       // flipperzero-like cmd  https://docs.flipper.net/development/cli/#wLVht
       // e.g. subghz tx 0000000000200001 868250000 403 10  // https://forum.flipper.net/t/friedland-libra-48249sl-wireless-doorbell-request/4528/20
       //                {hex_key} {frequency} {te} {count}
-      // subghz tx 0000000000445533 433920000 174 10
+      // subghz tx 445533 433920000 174 10
       const char* args = cmd_str.c_str() + strlen("subghz tx");
       uint64_t key=0;
       unsigned long frequency=433920000;
@@ -424,11 +447,71 @@ bool processSerialCommand(String cmd_str) {
     //esp_timer_stop(screensaver_timer);
     return true;
   }
+  
+  // gpio cmds https://docs.flipper.net/development/cli/#aqA4b
+  if(cmd_str.startsWith("gpio mode ")) {
+    const char* args = cmd_str.c_str() + strlen("gpio mode ");
+    int pin_number=-1;
+    int mode=0;
+    if (sscanf(args, "%d %d", &pin_number, &mode) == 2) {
+      // check usable pins according to the env
+      if(mode>=0 && mode<=1 && is_free_gpio_pin(pin_number)) {
+        pinMode(pin_number, mode);
+        return true;
+      }
+    }
+    // else
+    Serial.print("invalid args: ");
+    Serial.println(args);
+    return false;
+  }
+  if(cmd_str.startsWith("gpio set ")) {
+    const char* args = cmd_str.c_str() + strlen("gpio set ");
+    int pin_number=-1;
+    int value=0;
+    if (sscanf(args, "%d %d", &pin_number, &value) == 2) {
+      // check usable pins according to the env
+      if(value>=0 && value<=1 && is_free_gpio_pin(pin_number)) {
+        digitalWrite(pin_number, value);
+        return true;
+      }
+    }
+    // else
+    Serial.print("invalid args: ");
+    Serial.println(args);
+    return false;
+  }
+  if(cmd_str.startsWith("gpio read ")) {
+    const char* args = cmd_str.c_str() + strlen("gpio read ");
+    int pin_number=-1;
+    if (sscanf(args, "%d", &pin_number) == 1) {
+      // check usable pins according to the env
+      if(is_free_gpio_pin(pin_number)) {
+        Serial.println(digitalRead(pin_number));
+        return true;
+      }
+    }
+    // else
+    Serial.print("invalid args: ");
+    Serial.println(args);
+    return false;
+  }
 
-  // TODO: "storage" cmd to manage files  https://docs.flipper.net/development/cli/#Xgais
-
-  // TODO: "gpio" cmds  https://docs.flipper.net/development/cli/#aqA4b
-
+  // "storage" cmd to manage files  https://docs.flipper.net/development/cli/#Xgais
+  /*
+  if(cmd_str.startsWith("storage read ")) {
+    String txt = "";
+    String filepath = cmd_str.substring(strlen("storage read "), cmd_str.length());
+    filepath.trim();
+    if(!filepath.startsWith("/")) filepath = "/" + filepath;  // add "/" if missing
+    if(SD.exists(filepath)) txt = readSmallFile(SD, filepath);;
+    if(LittleFS.exists(filepath)) txt = readSmallFile(LittleFS, filepath);
+    if(txt.length()!=0) {
+      Serial.println(txt);
+      return true;
+    } else return false;
+  }*/
+ 
   //  TODO: more commands https://docs.flipper.net/development/cli#0Z9fs
 
   Serial.println("unsupported serial command: " + cmd_str);
