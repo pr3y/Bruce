370ec0121d9ba91d6b3b87a0846d7483069de8b1
Valentin <valentin.dole@protonmail.com>
2024-08-12 09:37:34 +0200

    Set Bruce name as pwnagotchi friend

--- src/modules/palnagotchi/pwngrid.cpp
+++ src/modules/palnagotchi/pwngrid.cpp
@@ -36,7 +36,7 @@ esp_err_t pwngridAdvertise(uint8_t channel, String face) {
   String pal_json_str = "";
 
   pal_json["pal"] = true;  // Also detect other Palnagotchis
-  pal_json["name"] = "Palnagotchi";
+  pal_json["name"] = "Bruce";
   pal_json["face"] = face;
   pal_json["epoch"] = 1;
   pal_json["grid_version"] = "1.10.3";
