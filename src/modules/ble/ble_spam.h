9a1fec95b00adf675a772aed9eb8a2224e50c79f
Pirata <bmorcelli@gmail.com>
2024-08-28 19:14:37 -0300

    .

--- src/modules/ble/ble_spam.h
+++ src/modules/ble/ble_spam.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <Arduino.h>
-#include <NIMBLEBeacon.h>
-#include <NIMBLEDevice.h>
-#include <NIMBLEUtils.h>
-#include <NIMBLEServer.h>
+#include <NimBLEBeacon.h>
+#include <NimBLEDevice.h>
+#include <NimBLEUtils.h>
+#include <NimBLEServer.h>
 #include <esp_gap_ble_api.h>
 void aj_adv(int ble_choice);
 
