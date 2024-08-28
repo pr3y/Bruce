1e66e47a6ddb8f6d7f1266f353e16de8195cd934
Pirata <bmorcelli@gmail.com>
2024-08-28 18:51:00 -0300

    Fix BLE Spoofer Fixed BLE Spoofer for iOS devices Grouped SourApple and AppleJuice in the same option, as they will perform almost the same thing Added NRF24 Jammer to Wifi Menu (yes, it jamms 2.4Ghz communications, including drones, Wifi and Bluetooth) Added delay to refrash clock and battery value on main screen enhanced interpreter Example1

--- src/modules/ble/ble_spam.h
+++ src/modules/ble/ble_spam.h
@@ -1,5 +1,9 @@
-#include <NimBLEDevice.h>
-#include <NimBLEBeacon.h>
-
+#pragma once
+#include <Arduino.h>
+#include <NIMBLEBeacon.h>
+#include <NIMBLEDevice.h>
+#include <NIMBLEUtils.h>
+#include <NIMBLEServer.h>
+#include <esp_gap_ble_api.h>
 void aj_adv(int ble_choice);
 
