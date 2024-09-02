6917c8e4eaf924d0e9cb73608658ebe8b8c7f8a7
Valou <valentin8709@users.noreply.github.com>
2024-09-02 12:59:09 +0200

    Add pwnagotchi & pwgrid spam credits

--- src/modules/pwnagotchi/mood.cpp
+++ src/modules/pwnagotchi/mood.cpp
@@ -1,3 +1,9 @@
+/* Thanks to thoses developers for their projects:
+* @7h30th3r0n3 : https://github.com/7h30th3r0n3/Evil-M5Core2 and https://github.com/7h30th3r0n3/PwnGridSpam
+* @viniciusbo : https://github.com/viniciusbo/m5-palnagotchi
+* @sduenasg : https://github.com/sduenasg/pio_palnagotchi
+*/
+
 #ifndef LITE_VERSION
 #include "mood.h"
 
