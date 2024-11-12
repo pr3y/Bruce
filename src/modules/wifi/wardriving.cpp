30f6c3c65cc4f06f70d180412d3e84204594552e
Shinichi Morimoto <s.morimoto@zest.jp>
2024-11-13 01:05:09 +0900

    fix CYD GPS

--- src/modules/wifi/wardriving.cpp
+++ src/modules/wifi/wardriving.cpp
@@ -42,7 +42,7 @@ void Wardriving::begin_wifi() {
 }
 
 bool Wardriving::begin_gps() {
-    GPSserial.begin(9600, SERIAL_8N1, SERIAL_RX, SERIAL_TX); // RX, TX
+    GPSserial.begin(9600, SERIAL_8N1, SERIAL_TX, SERIAL_RX);
 
     int count = 0;
     padprintln("Waiting for GPS data");
