87cd2aa051e3f22ec5543084369d643c47fe15c1
Pirata <bmorcelli@gmail.com>
2024-11-26 14:03:27 -0300

    CYD power Save fix

--- ports/CYD-2432S028/interface.cpp
+++ ports/CYD-2432S028/interface.cpp
@@ -178,6 +178,7 @@ bool checkPrevPress() {
 ** Verifies if Select or OK was pressed
 **********************************************************************/
 bool checkSelPress() { 
+    checkPowerSaveTime();
     if(menuPress(SEL)) {
         if(wakeUpScreen()) {
         delay(200);
