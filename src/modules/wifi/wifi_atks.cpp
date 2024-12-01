21d652017cde287fe49de25676e323fcb167c376
9dl <80607793+9dl@users.noreply.github.com>
2024-12-01 14:30:39 +0100

    fix: conditionally display action prompt based on touch capability

--- src/modules/wifi/wifi_atks.cpp
+++ src/modules/wifi/wifi_atks.cpp
@@ -82,7 +82,9 @@ void wifi_atk_info(String tssid, String mac, uint8_t channel)
   tft.drawString("AP: " + tssid, 10, 48);
   tft.drawString("Channel: " + String(channel), 10, 66);
   tft.drawString(mac, 10, 84);
-  tft.drawString("Press " + String(BTN_ALIAS) + " to act", 10, 102);
+  #if !defined(HAS_TOUCH)
+    tft.drawString("Press " + String(BTN_ALIAS) + " to act", 10, 102);
+  #endif
 
   delay(300);
   while (!checkSelPress())
