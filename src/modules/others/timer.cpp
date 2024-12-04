a25d3f158ddadcec3cb35ac2cae33b4d86a6d252
aleksei.gordienko <aleksei.gordienko@veeam.com>
2024-12-04 19:57:36 +0100

    updated a timer

--- src/modules/others/timer.cpp
+++ src/modules/others/timer.cpp
@@ -32,7 +32,7 @@ void timerLoop() {
         }
 
         unsigned long remainingMillis = duration - elapsedMillis;
-        int seconds = (remainingMillis / 1000) % 60;
+        int seconds = (remainingMillis / 1000) % 60 + 1;
         int minutes = (remainingMillis / 60000) % 60;
         int hours = (remainingMillis / 3600000);
 
@@ -44,7 +44,7 @@ void timerLoop() {
         tft.setTextSize(4);
         tft.drawCentreString(timeString, WIDTH / 2, HEIGHT / 2 - 13, 1);
 
-        if (checkSelPress() || checkEscPress()) {
+        if (checkEscPress()) {
             duration = 0;
             tft.fillScreen(bruceConfig.bgColor);
             returnToMenu = true;
@@ -59,9 +59,12 @@ void timerSetup() {
     int hours = 0;
     int minutes = 0;
     int seconds = 0;
+    int settingMode = 0;
+
     char timeString[9];
 
     tft.fillScreen(bruceConfig.bgColor);
+
     delay(300);
 
     for (;;) {
@@ -70,29 +73,51 @@ void timerSetup() {
         tft.setTextColor(bruceConfig.priColor, bruceConfig.bgColor);
         tft.setTextSize(2);
         tft.drawRect(10, 10, WIDTH - 15, HEIGHT - 15, bruceConfig.priColor);
-        tft.drawCentreString("Set Timer", WIDTH / 2, HEIGHT / 4 - 13, 1);
+        tft.drawCentreString("Set a timer", WIDTH / 2, HEIGHT / 4 - 13, 1);
         tft.setTextSize(4);
         tft.drawCentreString(timeString, WIDTH / 2, HEIGHT / 2 - 13, 1);
 
+        if (settingMode == 0) {
+            tft.drawLine(WIDTH / 10 * 7, HEIGHT / 3 * 2, WIDTH / 10 * 7 + tft.textWidth("00", 1), HEIGHT / 3 * 2, bruceConfig.bgColor);
+            tft.drawLine(WIDTH / 10, HEIGHT / 3 * 2, WIDTH / 10 + tft.textWidth("00", 1), HEIGHT / 3 * 2, bruceConfig.priColor);
+        } else if (settingMode == 1) {
+            tft.drawLine(WIDTH / 10, HEIGHT / 3 * 2, WIDTH / 10 + tft.textWidth("00", 1), HEIGHT / 3 * 2, bruceConfig.bgColor);
+            tft.drawLine(WIDTH / 10 * 4, HEIGHT / 3 * 2, WIDTH / 10 * 4 + tft.textWidth("00", 1), HEIGHT / 3 * 2, bruceConfig.priColor);
+        } else if (settingMode == 2) {
+            tft.drawLine(WIDTH / 10 * 4, HEIGHT / 3 * 2, WIDTH / 10 * 4 + tft.textWidth("00", 1), HEIGHT / 3 * 2, bruceConfig.bgColor);
+            tft.drawLine(WIDTH / 10 * 7, HEIGHT / 3 * 2, WIDTH / 10 * 7 + tft.textWidth("00", 1), HEIGHT / 3 * 2, bruceConfig.priColor);
+        }
+
         if (checkNextPress()) {
-            if (++seconds >= 60) {
-                seconds = 0;
-                if (++minutes >= 60) {
-                    minutes = 0;
-                    if (++hours > 99) hours = 0;
-                }
+            if (settingMode == 0) {
+                if (++hours > 99) hours = 0;
+            } else if (settingMode == 1) {
+                if (++minutes >= 60) minutes = 0;
+            } else if (settingMode == 2) {
+                if (++seconds >= 60) seconds = 0;
+            }
+        }
+
+        if (checkEscPress()) {
+            if (settingMode == 0) {
+                if (--hours < 0) hours = 99;
+            } else if (settingMode == 1) {
+                if (--minutes < 0) minutes = 59;
+            } else if (settingMode == 2) {
+                if (--seconds < 0) seconds = 59;
             }
         }
 
         if (checkSelPress()) {
-            duration = (hours * 3600 + minutes * 60 + seconds + 1) * 1000;
-            timerLoop();
-        }
+            if (++settingMode > 2 && (hours > 0 || minutes > 0 || seconds > 0)) {
+                duration = (hours * 3600 + minutes * 60 + seconds) * 1000;
+                timerLoop();
+                break;
+            }
 
-        if (checkEscPress()) {
-            tft.fillScreen(bruceConfig.bgColor);
-            returnToMenu = true;
-            break;
+            if (settingMode > 2 && hours == 0 && minutes == 0 && seconds == 0) {
+                settingMode = 0;
+            }
         }
 
         delay(200);
