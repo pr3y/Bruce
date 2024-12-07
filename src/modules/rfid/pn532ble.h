63f867895ddcf8491462931a089c606766c2ae70
whywilson <m.tools@qq.com>
2024-12-07 09:14:17 +0800

    Support Mifare Classic, ISO15693, EM4100 reading, writing and dump with PN532Killer.

--- src/modules/rfid/pn532ble.h
+++ src/modules/rfid/pn532ble.h
@@ -19,11 +19,21 @@ public:
 
     enum AppMode
     {
+        STANDBY_MODE,
         GET_FW_MODE,
-        HF_LOAD_DUMP_MODE,
-        HF_SCAN_MODE,
-        HF_DUMP_MODE,
-        HF_WRITE_MODE,
+        HF_MF_LOAD_DUMP_MODE,
+        HF_MFU_LOAD_DUMP_MODE,
+        HF_ISO15693_LOAD_DUMP_MODE,
+        HF_14A_SCAN_MODE,
+        HF_14B_SCAN_MODE,
+        HF_15_SCAN_MODE,
+        HF_MF_READ_MODE,
+        HF_MFU_READ_MODE,
+        HF_ISO15693_READ_MODE,
+        HF_MF_WRITE_MODE,
+        HF_MFU_WRITE_MODE,
+        HF_ISO15693_WRITE_MODE,
+        LF_EM4100_SCAN_MODE,
     };
 
 private:
@@ -34,16 +44,29 @@ private:
     void displayBanner();
     void showDeviceInfo();
     void hf14aScan();
-    void hf14aReadDump();
-    void hf14aWriteDump();
-    void hfmfWriteDump(ScrollableTextArea &area);
+    void hf15Scan();
+    void lfScan();
+    void hf14aMfReadDumpMode();
+    void hf14aMfWriteDumpMode();
+    void hf14aMfWriteDump(ScrollableTextArea &area);
+    void hf15ReadDumpMode();
+    void hf15WriteDumpMode();
     void selectMode();
+    void scanTagMenu();
+    void readTagMenu();
+    void writeDumpMenu();
+    void saveDumpMenu();
+    void loadDumpMenu();
     AppMode currentMode;
     void setMode(AppMode mode);
     uint8_t getMifareClassicSectorCount(uint8_t sak);
-    String saveMifareClassicDumpFile(std::vector<uint8_t> data, String uid);
+    String saveHfDumpBinFile(std::vector<uint8_t> data, String uid, String prefix);
     void loadMifareClassicDumpFile();
-    std::vector<uint8_t> dump;
+    void loadMifareUltralightDumpFile();
+    void loadIso15693DumpFile();
+    std::vector<uint8_t> mfd;
+    std::vector<uint8_t> mfud;
+    std::vector<uint8_t> iso15dump;
     std::string gen4pwd = "00000000";
     std::vector<uint8_t> default4bS20Config = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x09, 0x78, 0x00, 0x91, 0x02, 0xDA, 0xBC, 0x19, 0x10, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x04, 0x00, 0x09, 0x00};
     std::vector<uint8_t> default7bS20Config = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x09, 0x78, 0x00, 0x91, 0x02, 0xDA, 0xBC, 0x19, 0x10, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x44, 0x00, 0x09, 0x00};
