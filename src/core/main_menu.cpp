71553e79a32bd3e8f6f0a68c8a5e8f2fca21d68b
Rennan Cockles <r3ck.dev@gmail.com>
2024-12-07 23:42:50 -0300

    scale menu arrows

--- src/core/main_menu.cpp
+++ src/core/main_menu.cpp
@@ -65,22 +65,17 @@ void MainMenu::openMenuOptions(){
 ** Function name: draw
 ** Description:   Função para desenhar e mostrar o menu principal
 ***************************************************************************************/
-void MainMenu::draw() {
+void MainMenu::draw(float scale) {
     MenuItemInterface* current_menu = _menuItems[_currentIndex];
 
     drawMainBorder(false);
-    // Fix draw main menu icon remaining lines for those smaller than others
-    tft.fillRect(40, 40, WIDTH-70, HEIGHT-70, bruceConfig.bgColor);
-    tft.setTextSize(FG);
 
-    current_menu->draw();
+    current_menu->drawIcon(scale);
+    current_menu->drawArrows(scale);
 
     tft.setTextSize(FM);
-    tft.fillRect(10,30+80+(HEIGHT-134)/2, WIDTH-20,LH*FM, bruceConfig.bgColor);
+    tft.fillRect(10, 30+80+(HEIGHT-134)/2, WIDTH-20,LH*FM, bruceConfig.bgColor);
     tft.drawCentreString(current_menu->getName(), WIDTH/2, 30+80+(HEIGHT-134)/2, 1);
-    tft.setTextSize(FG);
-    tft.drawChar('<',10,HEIGHT/2+10);
-    tft.drawChar('>',WIDTH-(LW*FG+10),HEIGHT/2+10);
 
     #if defined(HAS_TOUCH)
     TouchFooter();
--- src/core/menu_items/MenuItemInterface.h
+++ src/core/menu_items/MenuItemInterface.h
@@ -2,21 +2,90 @@
 #define __MENU_ITEM_INTERFACE_H__
 
 #include "core/globals.h"
+#include "core/display.h"
 
 
 class MenuItemInterface {
 public:
     virtual ~MenuItemInterface() = default;
     virtual void optionsMenu(void) = 0;
-    virtual void draw(void) = 0;
+    virtual void drawIcon(float scale = 1) = 0;
     virtual String getName(void) = 0;
+
+    void drawArrows(float scale = 1) {
+        int arrowAreaX = BORDER_PAD_X;
+        int arrowAreaW = iconAreaX - arrowAreaX;
+
+        tft.fillRect(arrowAreaX, iconAreaY, arrowAreaW, iconAreaH, bruceConfig.bgColor);
+        tft.fillRect(WIDTH - arrowAreaX - arrowAreaW, iconAreaY, arrowAreaW, iconAreaH, bruceConfig.bgColor);
+
+        int arrowSize = scale * 10;
+        int lineWidth = scale * 3;
+
+        int arrowX = BORDER_PAD_X + arrowAreaW/4;
+        int arrowY = BORDER_PAD_Y + (HEIGHT - BORDER_PAD_Y) / 2;
+
+        // Left Arrow
+        tft.drawWideLine(
+            arrowX,
+            arrowY,
+            arrowX + arrowSize,
+            arrowY + arrowSize,
+            lineWidth,
+            bruceConfig.priColor,
+            bruceConfig.bgColor
+        );
+        tft.drawWideLine(
+            arrowX,
+            arrowY,
+            arrowX + arrowSize,
+            arrowY - arrowSize,
+            lineWidth,
+            bruceConfig.priColor,
+            bruceConfig.bgColor
+        );
+
+        // Right Arrow
+        tft.drawWideLine(
+            WIDTH - arrowX,
+            arrowY,
+            WIDTH - arrowX - arrowSize,
+            arrowY + arrowSize,
+            lineWidth,
+            bruceConfig.priColor,
+            bruceConfig.bgColor
+        );
+        tft.drawWideLine(
+            WIDTH - arrowX,
+            arrowY,
+            WIDTH - arrowX - arrowSize,
+            arrowY - arrowSize,
+            lineWidth,
+            bruceConfig.priColor,
+            bruceConfig.bgColor
+        );
+    }
 
 private:
     String _name = "";
 
 protected:
-    int iconX = WIDTH/2 - 40;
-    int iconY = 27 + (HEIGHT-134)/2;
+    int iconAreaH = (
+        (HEIGHT - 2*BORDER_PAD_Y) % 2 == 0
+        ? HEIGHT - 2*BORDER_PAD_Y
+        : HEIGHT - 2*BORDER_PAD_Y + 1
+    );
+    int iconAreaW = iconAreaH;
+
+    int iconCenterX = WIDTH/2;
+    int iconCenterY = HEIGHT/2;
+
+    int iconAreaX = iconCenterX - iconAreaW/2;
+    int iconAreaY = iconCenterY - iconAreaH/2;
+
+    void clearIconArea(void) {
+        tft.fillRect(iconAreaX, iconAreaY, iconAreaW, iconAreaH, bruceConfig.bgColor);
+    }
 };
 
 #endif
