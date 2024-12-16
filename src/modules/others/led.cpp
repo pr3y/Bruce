e26582e65e04dd522ce8a88222dd01bb7380670b
Cody Ardoin <cardoin@callbox.com>
2024-12-15 23:46:35 -0600

    Initial work on RGB LED

--- src/modules/others/led.cpp
+++ src/modules/others/led.cpp
@@ -7,19 +7,25 @@
 
 // Escolha o tipo de LED na lista abaixo.
 // Comente todos menos um LED_TYPE.
-// #define LED_TYPE        LED_STRIP_WS2812
-#define LED_TYPE        LED_STRIP_SK6812
+#ifdef T_EMBED_1101
+#define LED_TYPE    LED_STRIP_WS2812
+#define LED_COUNT   8
+#else
+#define LED_TYPE    LED_STRIP_SK6812
+#define LED_COUNT   1
+#endif
 // #define LED_STRIP_APA106
 // #define LED_STRIP_SM16703
 
-#define LED_TYPE_IS_RGBW 1  // Se o LED for do tipo RGBW, altere o 0 para 1
-#define LED_BRIGHT 245  // Define o brilho do LED. "0" está desligado; 255 pode queimar seus olhos (não recomendado)
+#define LED_TYPE_IS_RGBW 1  // Set to 1 if LED is RGBW
+#define LED_BRIGHT 245  // Set brightness. 0-255
+#define LED_BRIGHT_DEFAULT 245 
 
-LiteLED myLED( LED_TYPE, LED_TYPE_IS_RGBW );    // Cria o objeto LiteLED com o nome "myLED"
+LiteLED myLED( LED_TYPE, LED_TYPE_IS_RGBW );    // Create LiteLED Objected. Call it "myLED"
 
 void ledrgb_setup() {
-    myLED.begin( RGB_LED, 1 );         // Inicialize o objeto myLED. Aqui temos 1 LED conectado ao pino RGB_LED
-    myLED.brightness( LED_BRIGHT, 1 );  // Ligue o LED
+    myLED.begin( RGB_LED, LED_COUNT );         // Initialize the LED Object. Only 1 LED.
+    myLED.brightness( LED_BRIGHT, 1 );  // Set LED Brightness
 
     options = {
         {"OFF", [=]() { myLED.brightness( 0, 1 ); }},
@@ -35,21 +41,21 @@ void ledrgb_setup() {
 }
 
 void ledrgb_flash() {
-    myLED.begin( RGB_LED, 1 );         // Inicialize o objeto myLED. Aqui temos 1 LED conectado ao pino RGB_LED
-    myLED.brightness( LED_BRIGHT_DEFAULT, 1 );  // Ligue o LED
+    myLED.begin( RGB_LED, LED_COUNT );         // Inicialize o objeto myLED. Aqui temos 1 LED conectado ao pino RGB_LED
+    myLED.brightness( LED_BRIGHT, 1 );  // Ligue o LED
     myLED.setPixel( 0, L_PURPLE, 1 );
     delay(1000);
     myLED.brightness( 0, 1 );
     delay(1000);
-    myLED.brightness( LED_BRIGHT_DEFAULT, 1 );
+    myLED.brightness( LED_BRIGHT, 1 );
     delay(1000);
     myLED.brightness( 0, 1 );
     delay(1000);
-    myLED.brightness( LED_BRIGHT_DEFAULT, 1 );
+    myLED.brightness( LED_BRIGHT, 1 );
     delay(1000);
     myLED.brightness( 0, 1 );
     delay(1000);
-    myLED.brightness( LED_BRIGHT_DEFAULT, 1 );
+    myLED.brightness( LED_BRIGHT, 1 );
     delay(1000);
     myLED.brightness( 0, 1 );
 }
--- src/modules/others/led_control.h
+++ src/modules/others/led_control.h
@@ -3,17 +3,6 @@
 #include <Arduino.h>
 #include <LiteLED.h>
 
-// Escolha o tipo de LED na lista abaixo. Comente todos, exceto o que você está usando.
-#define LED_TYPE        LED_STRIP_SK6812
-// #define LED_STRIP_WS2812
-// #define LED_STRIP_APA106
-// #define LED_STRIP_SM16703
-
-#define LED_TYPE_IS_RGBW 1  // Se o LED for do tipo RGBW, altere o 0 para 1
-
-// Definição de intensidade de brilho
-#define LED_BRIGHT_DEFAULT 245  // Define o brilho padrão do LED. "0" é desligado; 255 pode ser muito brilhante.
-
 // Cores predefinidas para o LED
 static const crgb_t L_RED = 0xff0000;
 static const crgb_t L_GREEN = 0x00ff00;
