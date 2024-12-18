2db40bd92e0178a6960859f2274023de53a0e8d6
Cody Ardoin <cardoin@callbox.com>
2024-12-17 22:36:14 -0600

    Reapply "Header Guards - First Round"
    This reverts commit 09a9b188d785a003e0ed4dbeb690486839772e51.

--- src/modules/wifi/wigle.h
+++ src/modules/wifi/wigle.h
@@ -5,8 +5,8 @@
  * @version 0.1
  */
 
-#ifndef WIGLE_H
-#define WIGLE_H
+#ifndef __WIGLE_H__
+#define __WIGLE_H__
 
 #include "core/globals.h"
 #include <WiFiClientSecure.h>
@@ -39,4 +39,4 @@ private:
     bool _upload_file(File file, String upload_message);
 };
 
-#endif // WIGLE_H
+#endif
