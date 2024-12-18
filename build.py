ce6d64f4508c10d3dca9d8e8a8ccceb0e6a54c50
Cody Ardoin <cardoin@callbox.com>
2024-12-18 02:15:34 -0600

    Automatic creation of image for s3 mcu

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ Import("env")
 env.AddCustomTarget(
     "build-firmware",
     ["$BUILD_DIR/bootloader.bin","$BUILD_DIR/partitions.bin","$BUILD_DIR/firmware.bin"],
-    'pio pkg exec -p "tool-esptoolpy" -- esptool.py --chip esp32s3 merge_bin --output Bruce3_T-Embed_CC1101.bin 0x0 ${BUILD_DIR}\\bootloader.bin 0x8000 ${BUILD_DIR}\partitions.bin 0x10000 ${BUILD_DIR}\\firmware.bin'   
+    'pio pkg exec -p "tool-esptoolpy" -- esptool.py --chip esp32s3 merge_bin --output Bruce_${PIOENV}.bin 0x0 ${BUILD_DIR}\\bootloader.bin 0x8000 ${BUILD_DIR}\\partitions.bin 0x10000 ${BUILD_DIR}\\firmware.bin'   
 )
