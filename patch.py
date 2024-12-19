037655cd4933431e3369e551c36d2d680ef89031
Cody Ardoin <cardoin@callbox.com>
2024-12-19 07:16:55 -0600

    fix patch

--- patch.py
+++ patch.py
@@ -14,11 +14,11 @@ if not isfile(join(FRAMEWORK_DIR, ".patched")):
     original_file = join(FRAMEWORK_DIR, "tools", "sdk", mcu, "lib", "libnet80211.a")
     patched_file = join(FRAMEWORK_DIR, "tools", "sdk", mcu, "lib", "libnet80211.a.patched")
 
-    env.Execute("pio pkg exec -p toolchain-xtensa-%s -- xtensa-%s-elf-objcopy.exe  --weaken-symbol=s %s %s" % (mcu, mcu, original_file, patched_file))
+    env.Execute("pio pkg exec -p toolchain-xtensa-%s -- xtensa-%s-elf-objcopy  --weaken-symbol=s %s %s" % (mcu, mcu, original_file, patched_file))
     if(isfile("%s.old"%(original_file))):
         remove("%s.old"%(original_file))
     rename(original_file,"%s.old"%(original_file))
-    env.Execute("pio pkg exec -p toolchain-xtensa-%s -- xtensa-%s-elf-objcopy.exe  --weaken-symbol=ieee80211_raw_frame_sanity_check %s %s" % (mcu, mcu, patched_file, original_file))
+    env.Execute("pio pkg exec -p toolchain-xtensa-%s -- xtensa-%s-elf-objcopy  --weaken-symbol=ieee80211_raw_frame_sanity_check %s %s" % (mcu, mcu, patched_file, original_file))
     remove("%s.old"%(original_file))
 
     def _touch(path):
