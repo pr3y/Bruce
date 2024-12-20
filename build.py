b84060f39b6503715e3dacb452df445df5f040b2
Cody Ardoin <cardoin@callbox.com>
2024-12-20 04:22:23 -0600

    final touch up for build merge script

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 Import("env")
+
 env.AddCustomTarget(
     "build-firmware",
-    ["$BUILD_DIR/bootloader.bin","$BUILD_DIR/partitions.bin","$BUILD_DIR/firmware.bin"],
-    'pio pkg exec -p "tool-esptoolpy" -- esptool.py --chip ${BOARD_MCU} merge_bin --output Bruce_${PIOENV}.bin 0x0 ${BUILD_DIR}\\bootloader.bin 0x8000 ${BUILD_DIR}\\partitions.bin 0x10000 ${BUILD_DIR}\\firmware.bin'   
+    None,
+    'pio pkg exec -p "tool-esptoolpy" -- esptool.py --chip ${BOARD_MCU} merge_bin --output Bruce_${PIOENV}.bin 0x0 ${BUILD_DIR}\\bootloader.bin 0x8000 ${BUILD_DIR}\\partitions.bin 0x10000 ${BUILD_DIR}\\firmware.bin',
+    title="Merge Firmware Binaries",
+    always_build=False
 )
