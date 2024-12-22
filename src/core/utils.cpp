569fb3e95e445f8f93e0987397432ad956a12d55
Cody Ardoin <cardoin@callbox.com>
2024-12-21 20:39:07 -0600

    fixed conflicts

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -1,6 +1,28 @@
 #include "utils.h"
-#include "globals.h"
+#include <globals.h>
 #include "scrollableTextArea.h"
+/*********************************************************************
+**  Function: backToMenu
+**  sets the global var to be be used in the options second parameter
+**  and returnToMenu will be user do handle the breaks of all loops
+
+when using loopfunctions with an option to "Back to Menu", use:
+
+add this option:
+    options.push_back({"Main Menu", [=]() { backToMenu(); }});
+
+while(1) {
+    if(returnToMenu) break; // stop this loop and return to the previous loop
+
+    ...
+    loopOptions(options);
+    ...
+}
+*/
+
+void backToMenu() {
+  returnToMenu=true;
+}
 
 void updateClockTimezone(){
     timeClient.begin();
@@ -17,6 +39,10 @@ void updateClockTimezone(){
 #endif
 }
 
+void updateTimeStr(struct tm timeInfo) {
+  // Atualiza timeStr com a hora e minuto
+  snprintf(timeStr, sizeof(timeStr), "%02d:%02d:%02d", timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
+}
 
 void showDeviceInfo() {
     ScrollableTextArea area = ScrollableTextArea("DEVICE INFO");
