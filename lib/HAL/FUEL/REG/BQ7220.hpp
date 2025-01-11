7944a04a4d9d2d06f95df3143d9d3ee371f4dfac
Cody Ardoin <cardoin@callbox.com>
2025-01-10 23:30:46 -0600

    [FUEL] Prep

--- lib/HAL/FUEL/REG/BQ7220.hpp
+++ lib/HAL/FUEL/REG/BQ7220.hpp
@@ -69,84 +69,54 @@
 #define BQ27220_SUPER_KEY   0xffff
 
 #include <Arduino.h>
-#include <Wire.h>
-
-typedef enum OP_STATUS{
-    SEALED = 0b11,
-    UNSEALED = 0b10,
-    FULL = 0b01,
-};
-
-enum CURR_MODE
+namespace HAL::FUEL
 {
-    CURR_RAW,
-    CURR_INSTANT,
-    CURR_STANDBY,
-    CURR_CHARGING,
-    CURR_AVERAGE,
-};
-
-enum VOLT_MODE
-{
-    VOLT,
-    VOLT_CHARGING,
-    VOLT_RWA
-};
-
-union battery_state
-{
-    struct __st
+    typedef enum OP_STATUS
     {
-        uint16_t DSG : 1;
-        uint16_t SYSDWN : 1;
-        uint16_t TDA : 1;
-        uint16_t BATTPRES : 1;
-        uint16_t AUTH_GD : 1;
-        uint16_t OCVGD : 1;
-        uint16_t TCA : 1;
-        uint16_t RSVD : 1;
-        uint16_t CHGING : 1;
-        uint16_t FC : 1;
-        uint16_t OTD : 1;
-        uint16_t OTC : 1;
-        uint16_t SLEEP : 1;
-        uint16_t OCVFALL : 1;
-        uint16_t OCVCOMP : 1;
-        uint16_t FD : 1;
-    } st;
-    uint16_t full;
-};
+        SEALED = 0b11,
+        UNSEALED = 0b10,
+        FULL = 0b01,
+    };
 
-class BQ27220
-{
-public:
-    BQ27220();
-    bool unseal();
-    bool seal();
-    uint16_t getTemp();
-    uint16_t getBatterySt(void);
-    bool getIsCharging(void);
-    uint16_t getRemainCap();
-    uint16_t getTimeToEmpty();
-    uint16_t getFullChargeCap(void);
-    uint16_t getChargePcnt(void);
-    uint16_t getAvgPower(void);
-    uint16_t getStandbyCur(void);
-    uint16_t getVolt(VOLT_MODE type);
-    int16_t getCurr(CURR_MODE type);
-    uint16_t getId();
+    enum CURR_MODE
+    {
+        CURR_RAW,
+        CURR_INSTANT,
+        CURR_STANDBY,
+        CURR_CHARGING,
+        CURR_AVERAGE,
+    };
 
-private:
-    TwoWire *wire;
-    uint8_t addr;
-    int scl;
-    int sda;
-    union battery_state bat_st;
-    bool i2cReadBytes(uint8_t subAddress, uint8_t *dest, uint8_t count);
-    bool i2cWriteBytes(uint8_t subAddress, uint8_t *src, uint8_t count);
-    uint16_t readWord(uint16_t subAddress);
-    uint16_t readCtrlWord(uint16_t fun);
-    uint16_t writeCtrlWord(uint16_t fun);
-};
+    enum VOLT_MODE
+    {
+        VOLT,
+        VOLT_CHARGING,
+        VOLT_RWA
+    };
 
-#endif  /* __BQ7220__ */
+    union battery_state
+    {
+        struct __st
+        {
+            uint16_t DSG : 1;
+            uint16_t SYSDWN : 1;
+            uint16_t TDA : 1;
+            uint16_t BATTPRES : 1;
+            uint16_t AUTH_GD : 1;
+            uint16_t OCVGD : 1;
+            uint16_t TCA : 1;
+            uint16_t RSVD : 1;
+            uint16_t CHGING : 1;
+            uint16_t FC : 1;
+            uint16_t OTD : 1;
+            uint16_t OTC : 1;
+            uint16_t SLEEP : 1;
+            uint16_t OCVFALL : 1;
+            uint16_t OCVCOMP : 1;
+            uint16_t FD : 1;
+        } st;
+        uint16_t full;
+    };
+}
+
+#endif /* __BQ7220__ */
