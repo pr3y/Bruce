f8fc6482af124ad3c2571a05f32b8d0d29df9ff6
Lamn <70603474+Lamnxzp@users.noreply.github.com>
2025-02-01 01:15:29 +0100

    fix(custom_ir.h): parameter name

--- src/modules/ir/custom_ir.h
+++ src/modules/ir/custom_ir.h
@@ -16,7 +16,7 @@ void sendRC5Command(String address, String command);
 void sendRC6Command(String address, String command);
 void sendSamsungCommand(String address, String command);
 void sendSonyCommand(String address, String command);
-void sendKaseikyoCommand(String address, String data);
+void sendKaseikyoCommand(String address, String command);
 bool sendDecodedCommand(String protocol, String value, uint8_t bits=32);
 void otherIRcodes();
 bool txIrFile(FS *fs, String filepath);
