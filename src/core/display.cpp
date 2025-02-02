83a738e7e74d3e9431c07e2dc575681c78df3ab0
Tawank <kstawowski@gmail.com>
2025-02-02 12:47:35 +0100

    Fix in display.cpp

--- src/core/display.cpp
+++ src/core/display.cpp
@@ -553,7 +553,6 @@ void drawMainBorder(bool clear) {
     #endif
     if(clear){
       tft.fillScreen(bruceConfig.bgColor);
-      tft.fillScreen(bruceConfig.bgColor);
     }
     setTftDisplay(12, 12, bruceConfig.priColor, 1, bruceConfig.bgColor);
     tft.setTextDatum(0);
@@ -1082,18 +1081,10 @@ bool Gif::openGIF(FS *fs, const char *filename) {
       GIFDraw
     )
   ) {
-    Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif->getCanvasWidth(), gif->getCanvasHeight());
-    GIFINFO gi;
-    if (gif->getInfo(&gi)) {
-      Serial.printf("frame count: %d\n", gi.iFrameCount);
-      Serial.printf("duration: %d ms\n", gi.iDuration);
-      Serial.printf("max delay: %d ms\n", gi.iMaxDelay);
-      Serial.printf("min delay: %d ms\n", gi.iMinDelay);
-      return true;
-    }
+    return true;
   }
 
-  Serial.printf("GIF opening error: %d\n", gif->getLastError());
+  log_d("GIF opening error: %d\n", gif->getLastError());
   return false;
 }
 
@@ -1144,7 +1135,7 @@ bool showGif(FS *fs, const char *filename, int x, int y, bool center, int playDu
   long timeStart = millis();
   do {
     result = gif.playFrame(x, y);
-    if (result == -1) Serial.printf("GIF playFrame error: %d\n", gif.getLastError());
+    if (result == -1) log_d("GIF playFrame error: %d\n", gif.getLastError());
 
     if(check(AnyKeyPress)) break;
 
