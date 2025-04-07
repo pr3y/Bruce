0bca308d1b922f91539b724ce7328bcdffdeae04
Rennan Cockles <r3ck.dev@gmail.com>
2025-04-07 10:54:26 -0300

    create core/type_convertion helper

---
+++ src/core/type_convertion.h
@@ -0,0 +1,12 @@
+#ifndef __TYPE_CONVERTION_H__
+#define __TYPE_CONVERTION_H__
+
+#include <Arduino.h>
+
+String hexStrToBinStr(const String &hexStr);
+void decimalToHexString(uint64_t decimal, char *output);
+uint8_t hexCharToDecimal(char c);
+uint32_t hexStringToDecimal(const char *hexString);
+char *dec2binWzerofill(uint64_t Dec, unsigned int bitLength);
+
+#endif
