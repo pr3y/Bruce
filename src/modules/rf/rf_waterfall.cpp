f22640ec572275b9f25e4d40e68ef99a3835c019
Pirata <bmorcelli@gmail.com>
2025-05-21 22:49:46 -0300

    T-Embed and others Waterfall port

--- src/modules/rf/rf_waterfall.cpp
+++ src/modules/rf/rf_waterfall.cpp
@@ -130,11 +130,14 @@ void rf_waterfall_run() {
         for (int i = 0; i < screen_width; ++i) {
             float f_freq = f_start + i * f_freq_step;
             setMHZ(f_freq);
-            // To make sure CC1101 shared with TFT works properly
-            if (bruceConfigPins.CC1101_bus.mosi == TFT_MOSI) tft.drawPixel(0, 0, 0);
-            delayMicroseconds(100);
+            // To make sure CC1101 shared with TFT works properly on T-Embed
+            if (bruceConfigPins.CC1101_bus.mosi == TFT_MOSI) {
+                tft.drawPixel(0, 0, 0);
+                delayMicroseconds(150); // T-Embed case, need more time to process
+            } else delayMicroseconds(100);
+
             int i_rssi = ELECHOUSE_cc1101.getRssi();
-            // To make sure CC1101 shared with TFT works properly
+            // To make sure CC1101 shared with TFT works properly on T-Embed
             if (bruceConfigPins.CC1101_bus.mosi == TFT_MOSI) tft.drawPixel(0, 0, 0);
             if (i_rssi > temp_max_rssi) {
                 temp_max_rssi = i_rssi;
@@ -182,6 +185,7 @@ void rf_waterfall_run() {
                 delay(100);
             }
         }
+        tft.drawPixel(0, 0, 0); // Cardputer Case, need to call something to the tft.
         tft.pushImage(0, current_line, screen_width, 1, frameBuffer);
         tft.drawFastHLine(0, current_line + 1, screen_width, TFT_DARKGREY);
 
