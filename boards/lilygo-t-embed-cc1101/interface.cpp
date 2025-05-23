e2881d61752a527436d76c630147359b27705d68
Hs1r1us <29907973+Hs1r1us@users.noreply.github.com>
2025-05-23 23:46:10 +0800

    changed: t-embed-c1101 power acquisition

--- boards/lilygo-t-embed-cc1101/interface.cpp
+++ boards/lilygo-t-embed-cc1101/interface.cpp
@@ -119,12 +119,7 @@ void _setup_gpio() {
 int getBattery() {
     int percent = 0;
 #if defined(USE_BQ27220_VIA_I2C)
-    // percent=bq.getChargePcnt(); // this function runs bq.getRemainCap()/bq.getFullChargeCap()....
-    // bq.getFullChargeCap() is hardcoded int 3000.
-    // Serial.printf("Battery Capacity: %d\n", bq.getRemainCap());
-    percent = (int)((float)bq.getRemainCap() / (float)12.9);
-    // My battery is 1300mAh and bq.getRemainCap() doesn't go upper than
-    // 1290, that is why i'm dividing by 12.9 (var/1290)*100
+    percent = bq.getChargePcnt();
 #elif defined(T_EMBED)
     uint8_t _batAdcCh = ADC1_GPIO4_CHANNEL;
     uint8_t _batAdcUnit = 1;
