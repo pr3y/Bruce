0263b847de11fb38968a1f2ad723b0b7297beaaf
ben7147 <95097279+ben7147@users.noreply.github.com>
2025-05-28 23:26:06 +0200

    Add main files

---
+++ src/modules/Frequency_Gen/frequency.h
@@ -0,0 +1,31 @@
+#ifndef __BUZZER_H
+#define __BUZZER_H
+
+// class BuzzerConfig : public MenuItemInterface {
+// public:
+//     BuzzerConfig() : MenuItemInterface("Frequency") {}
+
+//     void optionsMenu(void);
+//     // void drawIcon(float scale);
+//     // void drawIconImg();
+//     // bool getTheme() { return bruceConfig.theme.others; }
+// };
+
+void Start11600_100();
+void Start11600_50();
+void Start11600_10();
+void Start11600_25();
+void Start11600_4();
+
+void BuzzerConfig();
+void BuzzerVolume();
+void StartFreq();
+void Start100();
+void Start50();
+void Start20();
+void Start10();
+void PlayErika();
+void playNeverGonnaGiveYouUp();
+void PlayNokia();
+
+#endif
