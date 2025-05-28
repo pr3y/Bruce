82b762a5d85ff75f195a971c246bfe363a8c1bd0
ben7147 <95097279+ben7147@users.noreply.github.com>
2025-05-28 23:40:11 +0200

    Update frequency.h
    removed unnecessary voids

--- src/modules/Frequency_Gen/frequency.h
+++ src/modules/Frequency_Gen/frequency.h
@@ -11,11 +11,6 @@
 //     // bool getTheme() { return bruceConfig.theme.others; }
 // };
 
-void Start11600_100();
-void Start11600_50();
-void Start11600_10();
-void Start11600_25();
-void Start11600_4();
 
 void BuzzerConfig();
 void BuzzerVolume();
@@ -24,8 +19,6 @@ void Start100();
 void Start50();
 void Start20();
 void Start10();
-void PlayErika();
-void playNeverGonnaGiveYouUp();
 void PlayNokia();
 
 #endif
