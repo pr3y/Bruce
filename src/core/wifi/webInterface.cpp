1119775a37483930390acf53abecb8cafce8a649
L Shaf <lshaf@users.noreply.github.com>
2025-06-07 01:30:13 +0700

    Enhancement for webInterface (#1342)
    * 🎗️ minify resource to have even smaller size
    
    * 👍 web interface issues
    
    - cm endpoint always give response 400 due to not handled by else
    - properly working logout
    - give response 200 for upload and give proper url to upload file
    
    * 🕸️ update webui and fix
    
    - add theme.css for webui color theme
    - fix minify only remove excessive spaces
    - better webui
    
    * ☔ add drop to upload

--- src/core/wifi/webInterface.cpp
+++ src/core/wifi/webInterface.cpp
@@ -210,10 +210,7 @@ void handleUpload(
         if (final) {
             // close the file handle as the upload is now done
             if (request->_tempFile) request->_tempFile.close();
-            request->redirect("/");
         }
-    } else {
-        return request->requestAuthentication();
     }
 }
 
@@ -260,6 +257,26 @@ void drawWebUiScreen(bool mode_ap) {
 }
 
 /**********************************************************************
+**  Function: color565ToWebHex
+**  convert 565 color to web hex format for theme purposes
+**********************************************************************/
+String color565ToWebHex(uint16_t color565) {
+    // Extract RGB components from 565
+    uint8_t r = (color565 >> 11) & 0x1F;
+    uint8_t g = (color565 >> 5) & 0x3F;
+    uint8_t b = color565 & 0x1F;
+
+    // Scale up to 8 bits
+    r = (r << 3) | (r >> 2);
+    g = (g << 2) | (g >> 4);
+    b = (b << 3) | (b >> 2);
+
+    char hex[8];
+    snprintf(hex, sizeof(hex), "#%02X%02X%02X", r, g, b);
+    return String(hex);
+}
+
+/**********************************************************************
 **  Function: configureWebServer
 **  configure web server
 **********************************************************************/
@@ -268,10 +285,17 @@ void configureWebServer() {
     DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
     server->onNotFound(notFound);
 
-    server->onFileUpload(handleUpload);
+    // server->onFileUpload(handleUpload);
+
+    server->on(
+        "/upload",
+        HTTP_POST,
+        [](AsyncWebServerRequest *request) { request->send(200, "text/plain", "File upload completed"); },
+        handleUpload
+    );
 
     server->on("/logout", HTTP_GET, [](AsyncWebServerRequest *request) {
-        AsyncWebServerResponse *response = request->beginResponse_P(302, "text/html", "", 0);
+        AsyncWebServerResponse *response = request->beginResponse_P(401, "text/html", "", 0);
         response->addHeader("Cache-Control", "no-cache, no-store, must-revalidate");
         response->addHeader("Location", "/logged-out");
         request->send(response);
@@ -308,6 +332,15 @@ void configureWebServer() {
                 request->beginResponse_P(200, "text/html", index_html, index_html_size);
             response->addHeader("Content-Encoding", "gzip");
             request->send(response);
+        } else {
+            request->requestAuthentication();
+        }
+    });
+    server->on("/theme.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+        if (checkUserWebAuth(request)) {
+            String css = ":root{--color:" + color565ToWebHex(bruceConfig.priColor) +
+                         ";--background:" + color565ToWebHex(bruceConfig.bgColor) + ";}";
+            request->send(200, "text/css", css);
         } else {
             request->requestAuthentication();
         }
@@ -399,8 +432,9 @@ void configureWebServer() {
             } else {
                 request->send(400, "text/plain", "command failed, check the serial log for details");
             }
+        } else {
+            request->send(400, "text/plain", "http request missing required arg: cmnd");
         }
-        request->send(400, "text/plain", "http request missing required arg: cmnd");
     });
 
     // Reinicia o ESP
