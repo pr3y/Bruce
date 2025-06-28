0b9c2e66063306c16997b97c0d6a47ef9f6967bc
andreock <andreockx@proton.me>
2025-06-28 17:52:13 +0200

    Remove old website

--- scripts/esp-web-tools-8.0.1/dist/components/ewt-button.d.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonBase } from "@material/mwc-button/mwc-button-base";
-declare global {
-    interface HTMLElementTagNameMap {
-        "ewt-button": EwtButton;
-    }
-}
-export declare class EwtButton extends ButtonBase {
-    static styles: import("lit").CSSResult[];
-}
--- scripts/esp-web-tools-8.0.1/dist/styles.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const dialogStyles: import("lit").CSSResult;
