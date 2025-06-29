e8a40a605dba383c23089b6094c3655f2e4d4133
pr3y <67231282+pr3y@users.noreply.github.com>
2025-06-28 22:11:58 -0300

    b

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,8 +41,8 @@
 <header class="fixed top-0 left-0 z-[100] w-full bg-[#111] py-3.5">
 	<div class="mx-auto w-11/12 max-w-full">
 		<nav class="flex items-center justify-between">
-			<a href={base} class="flex items-center">
-				<img src="/" alt="Bruce Logo" class="h-[50px]" />
+			<a href="/" class="flex items-center">
+				<img src="{base}/img/bruce.png" alt="Bruce Logo" class="h-[50px]" />
 			</a>
 			<button
 				class="inline-block text-white hover:text-purple-500 lg:hidden"
