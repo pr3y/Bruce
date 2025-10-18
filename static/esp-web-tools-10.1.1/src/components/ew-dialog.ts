3added2b7a731d1f89af0a3c53aef319eb7cf41f
Matt Emerick-Law <matt@emericklaw.co.uk>
2025-10-18 01:08:19 +0100

    Custom style for Bruce

--- static/esp-web-tools-10.1.1/src/components/ew-dialog.ts
+++ static/esp-web-tools-10.1.1/src/components/ew-dialog.ts
@@ -1,5 +1,6 @@
 import { Dialog } from "@material/web/dialog/internal/dialog.js";
 import { styles } from "@material/web/dialog/internal/dialog-styles.js";
+import { css } from "lit";
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -7,8 +8,21 @@ declare global {
   }
 }
 
+const customDialogStyles = css`
+:host {
+  --_hover-state-layer-opacity: 0.2;
+}
+  dialog {
+    border: 2px solid var(--md-sys-color-primary, #a020f0) !important;
+    border-radius: 12px !important;
+    box-shadow: 0px 0px 50px 50px rgba(0, 0, 0, 1);
+    -webkit-box-shadow: 0px 0px 50px 50px rgba(0, 0, 0, 1);
+    -moz-box-shadow: 0px 0px 50px 50px rgba(0, 0, 0, 1);
+  }
+`;
+
 export class EwDialog extends Dialog {
-  static override styles = [styles];
+  static override styles = [styles, customDialogStyles];
 }
 
 customElements.define("ew-dialog", EwDialog);
