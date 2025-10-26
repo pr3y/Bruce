ab66c18f927822ef7871dfceed9f58e2fc31618d
Matt Emerick-Law <matt@emericklaw.co.uk>
2025-10-26 04:13:30 +0000

    Formatting

--- lib/Bad_Usb_Lib/KeyboardLayout_pt_BR.cpp
+++ lib/Bad_Usb_Lib/KeyboardLayout_pt_BR.cpp
@@ -1,10 +1,10 @@
 /*
- * Standard US keyboard layout.
+ * Portuguese keyboard layout.
  */
 
 #include "KeyboardLayout.h"
 
-extern const uint8_t KeyboardLayout_en_US[128] PROGMEM = {
+extern const uint8_t KeyboardLayout_pt_BR[128] PROGMEM = {
     0x00, // NUL
     0x00, // SOH
     0x00, // STX
@@ -13,6 +13,7 @@ extern const uint8_t KeyboardLayout_en_US[128] PROGMEM = {
     0x00, // ENQ
     0x00, // ACK
     0x00, // BEL
+
     0x2a, // BS  Backspace
     0x2b, // TAB Tab
     0x28, // LF  Enter
@@ -32,7 +33,7 @@ extern const uint8_t KeyboardLayout_en_US[128] PROGMEM = {
     0x00, // CAN
     0x00, // EM
     0x00, // SUB
-    0x00, // ESC
+    0x29, // ESC
     0x00, // FS
     0x00, // GS
     0x00, // RS
@@ -40,20 +41,21 @@ extern const uint8_t KeyboardLayout_en_US[128] PROGMEM = {
 
     0x2c,         // ' '
     0x1e | SHIFT, // !
-    0x34 | SHIFT, // "
+    0x35 | SHIFT, // "
     0x20 | SHIFT, // #
     0x21 | SHIFT, // $
     0x22 | SHIFT, // %
-    0x24 | SHIFT, // &
-    0x34,         // '
+    0x23 | SHIFT, // &
+    0x35,         // '
     0x26 | SHIFT, // (
     0x27 | SHIFT, // )
     0x25 | SHIFT, // *
-    0x2e | SHIFT, // +
+    0x2E | SHIFT, // +
     0x36,         // ,
-    0x2d,         // -
+    0x2D,         // -
     0x37,         // .
-    0x38,         // /
+    0x54,         // /
+
     0x27,         // 0
     0x1e,         // 1
     0x1f,         // 2
@@ -64,13 +66,14 @@ extern const uint8_t KeyboardLayout_en_US[128] PROGMEM = {
     0x24,         // 7
     0x25,         // 8
     0x26,         // 9
-    0x33 | SHIFT, // :
-    0x33,         // ;
+    0x38 | SHIFT, // :
+    0x38,         // ;
     0x36 | SHIFT, // <
     0x2e,         // =
     0x37 | SHIFT, // >
-    0x38 | SHIFT, // ?
+    0x54 | SHIFT, // ? ???
     0x1f | SHIFT, // @
+
     0x04 | SHIFT, // A
     0x05 | SHIFT, // B
     0x06 | SHIFT, // C
@@ -97,41 +100,44 @@ extern const uint8_t KeyboardLayout_en_US[128] PROGMEM = {
     0x1b | SHIFT, // X
     0x1c | SHIFT, // Y
     0x1d | SHIFT, // Z
-    0x2f,         // [
-    0x31,         // bslash
-    0x30,         // ]
-    0x23 | SHIFT, // ^
+
+    0x30,         // [
+    0x64,         // bslash
+    0x31,         // ]
+    0x34 | SHIFT, // ^  not supported (requires dead key + space)
     0x2d | SHIFT, // _
-    0x35,         // `
-    0x04,         // a
-    0x05,         // b
-    0x06,         // c
-    0x07,         // d
-    0x08,         // e
-    0x09,         // f
-    0x0a,         // g
-    0x0b,         // h
-    0x0c,         // i
-    0x0d,         // j
-    0x0e,         // k
-    0x0f,         // l
-    0x10,         // m
-    0x11,         // n
-    0x12,         // o
-    0x13,         // p
-    0x14,         // q
-    0x15,         // r
-    0x16,         // s
-    0x17,         // t
-    0x18,         // u
-    0x19,         // v
-    0x1a,         // w
-    0x1b,         // x
-    0x1c,         // y
-    0x1d,         // z
-    0x2f | SHIFT, // {
-    0x31 | SHIFT, // |
-    0x30 | SHIFT, // }
-    0x35 | SHIFT, // ~
+    0x2f | SHIFT, // `  not supported (requires dead key + space)
+
+    0x04, // a
+    0x05, // b
+    0x06, // c
+    0x07, // d
+    0x08, // e
+    0x09, // f
+    0x0a, // g
+    0x0b, // h
+    0x0c, // i
+    0x0d, // j
+    0x0e, // k
+    0x0f, // l
+    0x10, // m
+    0x11, // n
+    0x12, // o
+    0x13, // p
+    0x14, // q
+    0x15, // r
+    0x16, // s
+    0x17, // t
+    0x18, // u
+    0x19, // v
+    0x1a, // w
+    0x1b, // x
+    0x1c, // y
+    0x1d, // z
+
+    0x30 | SHIFT, // {
+    0x64 | SHIFT, // |
+    0x31 | SHIFT, // }
+    0x34,         // ~  not supported (requires dead key + space)
     0x00          // DEL
 };
